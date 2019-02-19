d63f88ea35d7adc9c8213a3da66cf43a76698d57
yukitsuji <yukitsuji020832@gmail.com>
2019-02-19 11:27:00 -0800

    Update

--- hungarian.hpp
+++ hungarian.hpp
@@ -16,9 +16,9 @@ public:
                unsigned int n_rows, unsigned int n_cols);
 
 private:
-  int m_n_rows;
-  int m_n_cols;
-  int m_n_elements;
+  uint32_t m_n_rows;
+  uint32_t m_n_cols;
+  uint32_t m_n_elements;
   double m_dist_matrix[256 * 256] = {0};
   double m_assignment[256] = {0};
   bool m_covered_columns[256] = {};
@@ -27,18 +27,18 @@ private:
   bool m_prime_matrix[256 * 256] = {};
   bool m_new_star_matrix[256 * 256] = {};
 
-  void set_col_row(int n_rows, int n_cols) {
+  void set_col_row(uint32_t n_rows, uint32_t n_cols) {
     m_n_rows = n_rows;
     m_n_cols = n_cols;
     m_n_elements = n_rows * n_cols;
   }
   void assignmentoptimal();
-	void buildassignmentvector();
-	void step2a(int minDim);
-	void step2b(int minDim);
-	void step3(int minDim);
-	void step4(int minDim, int row, int col);
-	void step5(int minDim);
+  void buildassignmentvector();
+  void step2a(int minDim);
+  void step2b(int minDim);
+  void step3(int minDim);
+  void step4(int minDim, int row, int col);
+  void step5(int minDim);
 };
 
 #endif // FUSION_NODE__FUSION_NODE_HPP_
