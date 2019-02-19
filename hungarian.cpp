4b8931d12776e41f601e92adc93cc298fca7888f
yukitsuji <yukitsuji020832@gmail.com>
2019-02-19 11:30:31 -0800

    Update

--- hungarian.cpp
+++ hungarian.cpp
@@ -222,10 +222,10 @@ void HungarianAlgorithm::step3(int minDim)
 						m_prime_matrix[row + m_n_rows * col] = true;
 
 						/* find starred zero in current row */
-            int star_col = 0;
-						for (; star_col < m_n_cols; ++star_col) {
-							if (m_star_matrix[row + m_n_rows * star_col]) {
-								break;
+            int32_t star_col = 0;
+	    for (; star_col < m_n_cols; ++star_col) {
+	      if (m_star_matrix[row + m_n_rows * star_col]) {
+	        break;
               }
             }
 
